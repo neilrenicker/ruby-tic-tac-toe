68e328453263e41f8099f63350430c9bd997cfe5
Neil Renicker <neilrenicker@gmail.com>
2013-03-26 14:52:46 -0400

    finish primary game logic

--- game.rb
+++ game.rb
@@ -38,8 +38,47 @@ class Game
     [row1[2],row2[1],row3[0]]
   end
 
-  def row_win
-    row1.uniq.count == 1
+  def row_winner?
+    row1.uniq.count == 1 ||
+    row2.uniq.count == 1 ||
+    row3.uniq.count == 1
+  end
+
+  def column_winner?
+    col1.uniq.count == 1 ||
+    col2.uniq.count == 1 ||
+    col3.uniq.count == 1
+  end
+
+  def diagonal_winner?
+    diagonal_left.uniq.count == 1 ||
+    diagonal_right.uniq.count == 1
+  end
+
+  def winner_exists?
+    row_winner? ||
+    column_winner? ||
+    diagonal_winner?
+  end
+
+  def player1_wins
+    row1.uniq.count == 1 && row1.include?(1) ||
+    row2.uniq.count == 1 && row2.include?(1) ||
+    row3.uniq.count == 1 && row3.include?(1) ||
+    col1.uniq.count == 1 && col1.include?(1) ||
+    col2.uniq.count == 1 && col2.include?(1) ||
+    col3.uniq.count == 1 && col3.include?(1) ||
+    diagonal_left.uniq.count == 1 && diagonal_left.include?(1) ||
+    diagonal_right.uniq.count == 1 && diagonal_right.include?(1)
+  end
+
+  def winner
+    if player1_wins
+      "player1"
+    else
+      "player2"
+    end
   end
 
 end
+
--- spec/ttt_spec.rb
+++ spec/ttt_spec.rb
@@ -31,9 +31,45 @@ describe Game do
     game.diagonal_right.should == [0,2,2]
   end
 
-  it "should check for a winning top row" do
+  it "should check for a winner in a row" do
     game = Game.new([1,1,1],[0,2,0],[2,0,2])
-    game.row_win.should be_true
+    game.row_winner?.should be_true
+    game = Game.new([0,2,0],[0,2,0],[1,1,1])
+    game.row_winner?.should be_true
+  end
+
+  it "should determine if the row winner is player1 or player2" do
+    game = Game.new([1,1,1],[0,2,0],[2,0,2])
+    game.winner.should == "player1"
+  end
+
+  it "should check for a winner in a column" do
+    game = Game.new([1,0,1],[1,2,0],[1,2,2])
+    game.column_winner?.should be_true
+    game = Game.new([1,2,1],[1,2,0],[0,2,2])
+    game.column_winner?.should be_true
+  end
+
+  it "should determine if the column winner is player1 or player2" do
+    game = Game.new([1,0,1],[1,2,0],[1,2,2])
+    game.winner.should == "player1"
+  end
+
+  it "should check for a winner in the diagonals" do
+    game = Game.new([1,0,2],[2,1,0],[1,2,1])
+    game.diagonal_winner?.should be_true
+    game = Game.new([2,0,1],[2,1,0],[1,2,1])
+    game.diagonal_winner?.should be_true
+  end
+
+  it "should determine if the diagonal winner is player1 or player2" do
+    game = Game.new([2,0,1],[2,1,0],[1,2,1])
+    game.winner.should == "player1"
+  end
+
+  it "should declare whether a winner exists or not" do
+    game = Game.new([1,0,2],[2,1,0],[1,2,0])
+    game.winner_exists?.should be_false
   end
 
 end
