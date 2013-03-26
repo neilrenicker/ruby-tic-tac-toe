76185ae5376bc50180607f452b8cd9c74077f8d9
Neil Renicker <neilrenicker@gmail.com>
2013-03-26 10:07:33 -0400

    add row functionality

--- game.rb
+++ game.rb
@@ -1,3 +1,21 @@
 class Game
 
+  attr_accessor :score
+
+  def initialize(*score)
+    @score = score
+  end
+
+  def row1
+    score[0]
+  end
+
+  def row2
+    score[1]
+  end
+
+  def row3
+    score[2]
+  end
+
 end
--- spec/ttt_spec.rb
+++ spec/ttt_spec.rb
@@ -6,4 +6,16 @@ describe Game do
     game = Game.new
   end
 
+  it "should accept a score" do
+    game = Game.new([1,1,0],[0,2,0],[2,0,2])
+    game.score.should == [[1,1,0],[0,2,0],[2,0,2]]
+  end
+
+  it "should label the three rows" do
+    game = Game.new([1,1,0],[0,2,0],[2,0,2])
+    game.row1.should == [1,1,0]
+    game.row2.should == [0,2,0]
+    game.row3.should == [2,0,2]
+  end
+
 end
